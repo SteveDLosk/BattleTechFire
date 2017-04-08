d9b0444cc797859ac0d41a11ecbc2da297c86d68
Steve Losk <Stevedlosk@gmail.com>
2017-04-08 12:13:19 -0700

    Initial commit

---
+++ app/src/main/java/com/weebly/stevelosk/battletechfire/DiceRoll.java
@@ -0,0 +1,13 @@
+package com.weebly.stevelosk.battletechfire;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiceRoll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice_roll);
+    }
+}
